a7f7358ae730e06e0c07d5f86b41c45e3d8fc62a
Xatziri <11945627+Xatziri@users.noreply.github.com>
2023-12-04 15:23:53 -0700

    DayThree
    Day three includes dealing with arithmetic operations, addition, multiplication, division, and rounding doubles.

---
+++ DayThree/Solution.java
@@ -0,0 +1,50 @@
+package DayThree;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'solve' function below.
+     *
+     * The function accepts following parameters:
+     *  1. DOUBLE meal_cost
+     *  2. INTEGER tip_percent
+     *  3. INTEGER tax_percent
+     */
+
+    public static void solve(double meal_cost, int tip_percent, int tax_percent) {
+    // Write your code here
+    double mc = meal_cost;
+    int tp = tip_percent;
+    int txp = tax_percent;
+    double tc;
+    tc = (mc*tp/100)+(mc*txp/100)+mc;
+    int totalCost = (int) Math.round(tc);
+    System.out.print(totalCost);
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        double meal_cost = Double.parseDouble(bufferedReader.readLine().trim());
+
+        int tip_percent = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int tax_percent = Integer.parseInt(bufferedReader.readLine().trim());
+
+        Result.solve(meal_cost, tip_percent, tax_percent);
+
+        bufferedReader.close();
+    }
+}
