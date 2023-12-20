1c4cb7f6fbbb218aa62b5929bea720486a242caa
Xatziri <11945627+Xatziri@users.noreply.github.com>
2023-12-20 10:55:00 -0700

    DayTwenty
    DayTwenty includes decoding a matrix script that is a N x M grid of strings, and consists of alphanumeric characters, spaces and symbols (!,@,#,$,%,&).
    It reads each column, selects only the alphanumeric characters and connects them.
    If there are symbols or spaces between two alphanumeric characters of the decoded script, then they get replaced with a single space ' ' for better readability.

---
+++ DayTwenty/Matrix_Script.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+
+first_multiple_input = input().rstrip().split()
+
+n = int(first_multiple_input[0])
+
+m = int(first_multiple_input[1])
+
+matrix = []
+
+for _ in range(n):
+    matrix_item = input()
+    matrix.append(matrix_item)
+    
+transposed_matrix = list(map(list, zip(*matrix)))
+decoded_columns = [''.join(column) for column in transposed_matrix]
+decoded_script = ''.join(decoded_columns)
+decoded_script = re.sub(r'(?<=[a-zA-Z0-9])[^a-zA-Z0-9]+(?=[a-zA-Z0-9])', ' ', decoded_script)
+  
+print(decoded_script)
+
