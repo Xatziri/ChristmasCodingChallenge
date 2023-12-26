c4e460371d5b15f741cd6ff13f68251c59bf1c87
Xatziri <11945627+Xatziri@users.noreply.github.com>
2023-12-25 23:28:29 -0700

    Update
    The update includes renaming the card_number variable to a different name so the function is_valid_credit_card can use the original input with the hyphens to search for the criteria that validates that the number is grouped by 4 digits and the input with removed hyphens  to search for the criteria that validates all other cases.

--- DayTwentyFive/Solution.py
+++ DayTwentyFive/Solution.py
@@ -5,20 +5,24 @@ def is_valid_credit_card(card_number):
     # Check if it starts with 4, 5, or 6
     if not re.match(r'^[4-6]', card_number):
         return 'Invalid'
-  
-    # Remove hyphens and check if it contains only digits
-    card_number = card_number.replace('-', '')
-    if not card_number.isdigit():
-        return 'Invalid'
 
+    # Remove hyphens and check if it contains only digits
+    card_number_cleaned = card_number.replace('-', '')
+    
     # Check if it has exactly 16 digits
-    if not re.match(r'^\d{16}$', card_number):
+    if not re.match(r'^\d{16}$', card_number_cleaned):
         return 'Invalid'
 
     # Check if it has 4 or more consecutive repeated digits
-    if re.search(r'(\d)\1{3,}', card_number):
+    if re.search(r'(\d)\1{3,}', card_number_cleaned):
         return 'Invalid'
-
+    
+    # Check if it contains hyphens and they are grouped by four digits
+    if '-' in card_number:
+        groups = card_number.split('-')
+        if any(len(group) != 4 for group in groups):
+            return 'Invalid'
+    
     return 'Valid'
 
 # Input
